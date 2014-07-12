17d622713f1cadcf7b849d5876bfb921563fbe0b
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-07-12 14:45:43 +0530

    Adds command line argument for what to print.

--- trial0.c
+++ trial0.c
@@ -1,9 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int
 main(int argc, char *argv[])
 {
-  printf("%s\n", argv[1]);
+  char *s;
+  if (argc == 2) {
+    s = argv[1];
+  } else {
+    s = (char *)malloc(sizeof(char) * 13);
+    strcpy(s, "Hello, world");
+  }
+  printf("%s\n", s);
   return 0;
 }
 
